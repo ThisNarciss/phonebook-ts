eab7772310ad7c70abca37314647082cd80b2eb3
ThisNarciss <tyomaxxx000@gmail.com>
2023-06-19 12:13:48 +0300

    delete path

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react';
 // import css from 'vite-plugin-css';
 
 export default defineConfig({
-  base: '/phonebook-ts/',
   plugins: [react()],
   resolve: {
     alias: {
